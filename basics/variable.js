d079505d95e45551d3805b7945e9f0a1175ae058
Priyansh Singh <priyansh.singh211@gmail.com>
2025-09-01 18:11:31 +0530

    container config done

---
+++ basics/variable.js
@@ -0,0 +1,10 @@
+const accountId = 12232;
+let accountEmail = "priyansh.singh211@gmail.com";
+var accountPass = "6363"
+let accountCity = "jaipur"
+
+// accountId = 2;  you cant change the const variable
+
+console.log(accountEmail);
+
+console.table([accountId, accountPass, accountEmail, accountCity]);
