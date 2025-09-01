0ee9a7da8678a611d08345126e67db6c82c3d3bf
Priyansh Singh <priyansh.singh211@gmail.com>
2025-09-01 18:49:14 +0530

    container config done

---
+++ basics/datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; //treat all JS code as newer version;
+
+let age = 21;
+let name = "priyansh";
+let half = true;
+
+console.log(typeof "Priyansh");
+console.log(typeof null); //object
+
+console.log(typeof undefined); //undefined
