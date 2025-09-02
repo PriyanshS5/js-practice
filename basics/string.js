a1e9c84b5a7df7d2e305ce7f71c0b0d9d1b4e829
Priyansh Singh <priyansh.singh211@gmail.com>
2025-09-02 23:21:54 +0530

    container config done

---
+++ basics/string.js
@@ -0,0 +1,16 @@
+const name = "Priyansh";
+const repoCount = 50;
+
+//console.log(name + repoCount + " value");
+
+console.log(`I am learning to be the best. My repoCount is ${repoCount}..Dhantene ${name}`);
+const newName = new String("Ankit");
+console.log(newName);
+console.log(newName.length);
+console.log(newName.toUpperCase());
+
+const anotherStr = newName.slice(-1,4);
+console.log(anotherStr);
+
+const anotherStr1 = newName.substring(0,2);
+console.log(anotherStr1);
