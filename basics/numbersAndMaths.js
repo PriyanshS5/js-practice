861f7d5d839e26a178b86c37e046d094e4b8590f
Priyansh Singh <priyansh.singh211@gmail.com>
2025-09-04 23:21:04 +0530

    container config done

--- basics/numbersAndMaths.js
+++ basics/numbersAndMaths.js
@@ -6,3 +6,6 @@ console.log(newNumber);
 
 console.log(newNumber.toString()); // converting it to string which helps it to access string functions which are available to perform at this object
 console.log(newNumber.toFixed(2)); //it gives a precise decimal value after it
+
+const other = 123.88;
+console.log(other.toPrecision(3)); // it gives a precise value of the given digits so be carefull
