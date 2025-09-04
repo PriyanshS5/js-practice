1158cb48345ba30a6a07d570c04c0565ca63acf3
Priyansh Singh <priyansh.singh211@gmail.com>
2025-09-04 23:16:43 +0530

    container config done

--- basics/numbersAndMaths.js
+++ basics/numbersAndMaths.js
@@ -5,3 +5,4 @@ let newNumber = new Number(20);  // protoype values for numbers
 console.log(newNumber);
 
 console.log(newNumber.toString()); // converting it to string which helps it to access string functions which are available to perform at this object
+console.log(newNumber.toFixed(2)); //it gives a precise decimal value after it
