8add1cbc7cd41f130f103dac1db344725cd90da1
Priyansh Singh <priyansh.singh211@gmail.com>
2025-09-05 23:49:45 +0530

    container config done

--- basics/DateAndTime.js
+++ basics/DateAndTime.js
@@ -1,7 +1,13 @@
 let myDate = new Date();
-console.log(myDate);
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toISOString());
-console.log(myDate.toLocaleDateString());
-console.log(myDate.toLocaleString());
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+//console.log(typeof(myDate));
+
+const newDate = new Date(2022, 1, 30);
+console.log(newDate.toDateString());
+console.log(newDate.toLocaleString());
